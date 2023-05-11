cd660729280a5e3b6a0f9297010bf8830be8add1
Дрогаль Андрей <ddr533@yandex.ru>
2023-05-12 03:22:39 +0700

    Del init.pu from api/test, fix imports.

--- api_yamdb/api/tests/test_review.py
+++ api_yamdb/api/tests/test_review.py
@@ -2,9 +2,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
 
-from reviews.models import (User, Genre, Category, Title,
-from review.models import (User, Genre, Category, Title,
-                           Review, Comment, GenreTitle)
+from reviews.models import User, Genre, Category, Title, Review
 
 
 class TestMyAPI(APITestCase):
