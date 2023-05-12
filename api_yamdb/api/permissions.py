264299e3a516cfbfd5b4100453dd9294bcedd213
Степан Васильев <stepanvasilyev00@gmail.com>
2023-05-12 12:44:06 +0300

    Add unittest for genres, categories, titles

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -28,21 +28,20 @@ class UserMePermissions(permissions.IsAuthenticated):
         return request.user.username == obj.user.username
 
 
-
 class IsAuthorOrStaffOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
     """Изменять контент может только автор, администратор, модератор."""
 
     message = 'Изменять контент может только его автор, модератор или админ.'
 
     ALLOW_EDIT = ('moderator', 'admin')
-    
+
     def has_object_permission(self, request, view, obj):
 
         return any((request.method in permissions.SAFE_METHODS,
                     request.user == obj.author,
                     request.user.is_superuser,
-                    request.user.is_authenticated and
-                    request.user.role in self.ALLOW_EDIT))
+                    request.user.is_authenticated
+                    and request.user.role in self.ALLOW_EDIT))
 
 
 class AdminOrReadOnly(permissions.BasePermission):
@@ -51,14 +50,14 @@ class AdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS
-                or (request.user.is_authenticated
-                    and request.user.role == 'admin')
+            request.method in permissions.SAFE_METHODS
+            or (request.user.is_authenticated
+                and request.user.role == 'admin')
+            or request.user.is_superuser
         )
 
     def has_object_permission(self, request, view, obj):
         return any((request.method in permissions.SAFE_METHODS,
                     request.user.is_superuser,
-                    request.user.is_authenticated and
-                    request.user.role == 'admin'))
-
+                    request.user.is_authenticated
+                    and request.user.role == 'admin'))
